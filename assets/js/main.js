fb3ddfb64e2c94fcb987e5d2f4b962a08c60e83c
damirpopo <43522476+damirpopo@users.noreply.github.com>
2022-11-14 14:29:58 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+// mas=['Привет, ', 'мир','!'];
+// alert(mas)
+
+// mas=['Привет, ', 'мир','!'];
+// let text=mas
+// alert(text)
+
+// mas=['Привет, ', 'мир','!'];
+// mas.unshift('пока')
+// alert(mas)
+
+
+// var obj={коля: '200', петя: '400'};
+// for (let i in obj) {
+//     console.log(`${i}- зарплата ${obj[i]} долоров`);
+//   }
+
+// var arr={
+//     'en':['blue','red','green'],
+//     'ru':['голубой','красный','зелёный'],
+// };
+// alert(arr.ru[1]);
+
+// arr = ['a', 'b', 'c']
+// alert(arr)
+
+// arr = ['a', 'b', 'c']
+// alert(arr)
+
+// arr = ['a', 'b', 'c', 'd'] 
+// alert(`'${arr[0]}+${arr[1]}, ${arr[2]}+${arr[3]}'`)
+
+// var arr =[2, 5, 3, 9]
+// var result=(arr[0]+arr[1])*(arr[2]+arr[3])
+// alert(result)
+
+// var obj = {a: 1, b: 2, c: 3};
+// alert(obj.c)
+
+// var obj={Коля: '1000', Вася: '500', Петя: '200'};
+// alert(obj.Коля)
+// alert(obj.Вася)
+// alert(obj.Петя)
+
+// var obj={Понедельник: '1', вторник:'2',среда:'3',четверг:'4',пятнеца:'5',субота:'6',воскресенье:'7'}
+// alert(obj.вторник)
+
+// let day=['1','2','3','4','5','6','7']
+// var obj={Понедельник: day[0], вторник:day[1],среда:day[2],четверг:day[3],пятнеца:day[4],субота:day[5],воскресенье:day[6]}
+// alert(obj.Понедельник)
+
+// var arr=[ [1, 2, 3], [4, 5, 6], [7,8,9] ]
+// alert(arr[1][0])
+
+// var obj={js:['jQuery', 'Angular'], php: 'hello', css: 'world'}
+// alert(obj.js[0])
+
+// var arr={
+//     'en':['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+//     'ru':['понедельник', 'вторник', 'среда', 'четверг', 'пятница' ,'суббота', 'воскресенье'],
+// };
+// alert(arr.ru[0])
+// alert(arr.en[2])
+
+// let lang = prompt( 'ru или en', '' );
+// let day = prompt( 'день недели цифрой?', '' );
+// let obj = {ru: {1: 'Понедельник', 2: 'Вторник', 3: 'Среда', 4: 'Четверг', 5: 'Пятница', 6: 'Суббота', 7: 'Воскресенье'}, en: {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'}};
+// alert( obj[lang][day] );
